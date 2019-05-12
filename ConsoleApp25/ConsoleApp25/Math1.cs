dbbd1587b82d13b8647ee72ba3e4f110eb876bac
Amir <amirkkvnd99@gmail.com>
2019-05-13 01:35:32 +0430

    Added the full project file

---
+++ ConsoleApp25/ConsoleApp25/Math1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    static class Math1
+    {
+        public static int a { get; set; }
+
+        public static void GetPow(double a)
+        {
+            Console.WriteLine(a * a);
+        }
+    }
+}
---
+++ ConsoleApp25/ConsoleApp25/Math2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Math2
+    {
+        public static int a { get; set; }
+
+        public void GetPow(double a)
+        {
+            Console.WriteLine(a * a);
+        }
+    }
+}
